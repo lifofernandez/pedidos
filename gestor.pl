af0fc6b9899ea3c055aeed8d566b809672d909ee
lifo fernandez <lifofernandez@gmail.com>
2016-07-10 01:30:21 -0300

    reservar sub

--- gestor.pl
+++ gestor.pl
@@ -32,7 +32,7 @@ my %registros = %$registroJson;
 informeReservas();
 
 foreach (@pedidos){
-	#chomp;
+	# chomp;
 	if( $_ =~ /^\s*item,mes,/ ){ # borrrar primera linea
 		next;
 	}
@@ -41,11 +41,10 @@ foreach (@pedidos){
 
 informeReservas();
 
-
-
 # Subs
 sub consultar{
 	my ($item,$mes,$dia,$hora,$duracion) =  split /\W/, $_;
+
 	my $pedidoItem = {
 		# item =>		$item,
 		mes => $mes,
@@ -57,6 +56,7 @@ sub consultar{
 	# AGREGAR CAMPOS: COMENTARIO Y FAMILIA/TIPO
 	# print Dumper($pedidoItem);
 	header($item);
+
 	if($item ~~ @inventario){ # en inventario?
 		say "Ingresando pedido: $item $mes $dia $hora $duracion";
 
@@ -65,7 +65,6 @@ sub consultar{
 			my $nReservasItem = scalar @reservasItem;
 			say "| Existen $nReservasItem reservas registrdas para: $item";
 
-			# Encapsular !!!!
 			# MATRIX de reservas que tiene hasta el momento el item
 			# Es mejor guardarlo en registro para no repetir
 			# YA VA DECANTAR CUANDO GRABE ESTOS REGISTROS
@@ -79,49 +78,78 @@ sub consultar{
 				$reservasMatrix{$m}{$d}{$h} = $l;
 			}
 
-
-
 			# Buscar lugar libre en la matriz...
+			# encapsular (matriz, pedido) para poder hacer recursivo
+
+			# en duraciones mayores a 1
 			if($mes ~~ %reservasMatrix){
-				say "| Mes: $mes ocupado, voy a buscar en los dias:";
+				say "| Mes: $mes ocupado, voy a buscar en los dias...";
 
 				if($dia ~~ %{$reservasMatrix{$mes}}){
-					say "|| Dia: $dia ocupado, voy q buscar en las horas:";
+					say "|| Dia: $dia ocupado, voy q buscar en las horas...";
 
 					if($hora ~~ %{$reservasMatrix{$mes}{$dia}}){
-						say "||| Hora: $hora ocupada, hay q buscar otra fecha disponible:";
+						say "||| Hora: $hora ocupada, hay q buscar otra fecha disponible...";
 
 						# LISTO hasta aca tengo QUE:
 						# * Mostrar algo como el proximo dia mes hora disponible
 						#   AKA Cuando vuelve el item
+						# ??? continuar ???
 
 					}else{
 						say "||| Hora: $hora libre!";
 
+						# ??? Ordenar la matriz ???
+						# ??? para consular horas/tiempo hasta proxima reserva ???
+						# para preguntar: Quiere guardar la reserva ( tenga en
+						# cuenta debera devolver el item antes... )?
 
-						# Antes de agregar tengo QUE:
-						# * Ver si la duración no pisa otra reserva
-						#   Dar opción o cortar y avisar..
+						# Si dice que SI, recursivear hasta que "choque"
+						# con una hora ocupada y ?continue?.
+						# ??? poner limite de 24 horas ???
 
-						reservar($registros{$item}{reservas}, $pedidoItem);
-						say "continuo."; next;
-						# Despues / ademas de agregar tengo QUE:
-						# * RESERVAR proximas {horas} a partir de la duracion
-						# for i < $l
-						# 	%reservas mes dia {i} = 0 # marcar / inhabilitar
+						# Si dice que NO, exit!
+
+						# armar ARRAY de PEDIDOS X proximas horas
+						# desde ahora hasta $duracion
+
+						# CALCULAR SALTOS DE DIA {$h+i%24}
+
+						# Y PASAR CADA hora POR LA MATRIX para reservar
+						# wile(@PEDIDOS < duracion)){
+						# 	reservarHora(matrix, pedido)
+						# 		= $duracion - $delta (rampa hasta que libera ;)
 						# }
+
+						##########################################################
+						# poniendo valores de durcion decendentes (duracion - i )
+						#  qeu hay entre el hago que cuqndo sea 1 termine el loop
+
+
+
+
+						# Evaluar duracion:
+						if($duracion > 1){
+							say "Duracion mayor q 1... ";
+							say "(deberia reservar las proximas $duracion hs)";
+							say "por ahora, continuo."; next;
+						}else{
+							# Antes de agregar tengo QUE:
+							reservarHora($registros{$item}{reservas}, $pedidoItem);
+							say "continuo."; next;
+						}
 					}
 
 				}else{
 					say "|| Dia: $dia libre!";
-					reservar($registros{$item}{reservas}, $pedidoItem);
+					reservarHora($registros{$item}{reservas}, $pedidoItem);
 					say "continuo."; next;
 					next;
 				}
 
 			}else{
 				say "| Mes: $mes libre!";
-				reservar($registros{$item}{reservas}, $pedidoItem);
+				reservarHora($registros{$item}{reservas}, $pedidoItem);
 				say "continuo."; next;
 				next;
 			} # Termina matrix
@@ -129,7 +157,7 @@ sub consultar{
 		}else{
 			say "| No se encontraron reservas para: $item";
 			$registros{$item} = {"reservas" => [$pedidoItem]}; # Revisar estructura
-			say "Creo primer registro, continuo."; next;
+			say "Creo primer registro para este item,\ncontinuo."; next;
 		}
 
 	}else{
@@ -140,11 +168,8 @@ sub consultar{
 
 # Subs
 
-sub reservar {
 
-	# Antes de agregar tengo QUE:
-	# * Ver si la duración no pisa otra fecha
-	#   Dar opción o cortar y avisar ?
+sub reservarHora {
 
 	my $p = $_[-1];
 	push $_[0], $p;
